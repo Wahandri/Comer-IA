c0fec4d54f55e696ce1d41842c5b03a25c95337f
Wahandri <wahandricode@gmail.com>
2025-02-11 17:55:39 +0100

    Ajustar estilos de entrada de ingredientes y agregar encabezado a la página principal

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,25 @@
 import IngredientInput from '../components/IngredientsInput/IngredientsInput';
 import Footer from '../components/Footer/Footer';
+import Head from 'next/head';
 
 
 export default function Home() {
   return (
-    <main className="container">
-      <img src='/logoComerIA.png' width='400px' alt="Comer-IA Logo" className="logo" />
-      {/* <h1 className="title">Bienvenido</h1> */}
-      <p className="description">Introduce tus ingredientes y parametros</p>
-      <p className="description">Una IA creara una receta para ti.</p>
-      <div className='box-Ingrdients'>
-        <IngredientInput />
-      </div>
-    <Footer />
-    </main>
+    <>
+      <Head>
+        <title>Comer-IA</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="container">
+        <img src='/logoComerIA.png' width='400px' alt="Comer-IA Logo" className="logo" />
+        {/* <h1 className="title">Bienvenido</h1> */}
+        <p className="description">Introduce tus ingredientes y parametros</p>
+        <p className="description">Una IA creara una receta para ti.</p>
+        <div className='box-Ingrdients'>
+          <IngredientInput />
+        </div>
+        <Footer />
+      </main>
+    </>
   );
 }
