5a8fc8ae2c870e1029c5d4c1d910b4da5e37d15f
Wahandri <wahandricode@gmail.com>
2025-03-02 01:42:07 +0100

    Actualizar estilos globales y cambiar el logo a formato GIF en la página principal

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="container">
-        <img src='/logoComerIA.png' width='400px' alt="Comer-IA Logo" className="logo" />
+        <img src='/logoComerIA.gif' width='400px' alt="Comer-IA Logo" className="logo" />
+        <h1>Comer-IA</h1>
         {/* <h1 className="title">Bienvenido</h1> */}
         <p className="description">Introduce tus ingredientes y parametros</p>
         <p className="description">Una IA creara una receta para ti.</p>
