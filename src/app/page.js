b149131593dcdf4e1800a8e459815a68573ba77e
Wahandri <wahandricode@gmail.com>
2025-03-05 21:40:47 +0100

    Agregar estilos para el título de la página y mejorar la estructura del componente principal

--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,17 @@ export default function Home() {
         <title>Comer-IA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <main className="container">
         <img src='/logoComerIA.gif' width='300px' alt="Comer-IA Logo" className="logo" />
-        <h1>Comer-IA</h1>
+        <div>
+          <h1>Comer-IA</h1>
+          <h1 className='h1Page'>Crea recetas con inteligencia artificial</h1>
+        </div>
+
         <p className="description">Introduce tus ingredientes y parametros.<br/> Una IA creara una receta para ti.</p>
+
+        
         <div className='box-Ingrdients'>
           <IngredientInput />
         </div>
